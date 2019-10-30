beb42678eacc1dfc9597dfe534577c6bc8680158
Javier Alejandro <javiagl98@gmail.com>
2019-10-29 23:35:05 -0600

    version 3

---
+++ routes/Pila.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+const data = require('./Data.json');
+ // metodo get
+
+ router.get('/GetData',function(req,res,next){
+    res.json(data);
+ });
