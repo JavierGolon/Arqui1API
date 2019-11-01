c380064e0cfa92e51477380f4a1f5ad3a4ab97d7
Javier Alejandro <javiagl98@gmail.com>
2019-10-31 23:31:35 -0600

    version 5

--- routes/Pila.js
+++ routes/Pila.js
@@ -8,4 +8,18 @@ const data = require('./Data.json');
     res.json(data);
  });
 
+ // metodo Post
+
+ router.post('/PostData',(req,res) =>{
+   const {Tipo,Contenido}= req.body;
+   if(Tipo,Contenido){
+      const newdata = {...req.body};
+      data.push(newdata);
+      res.json(data);
+
+   }else{
+      res.status(500).json({error:'There was an error'});
+   }
+ });
+
  module.exports = router;
